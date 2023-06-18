06fc2b055c0e4dae01bd255593879c3a8e2751ad
Aleksandr_M <9664222@gmail.com>
2023-06-18 16:36:57 +0300

    demoqa_home1

---
+++ pages/swag_labs.py
@@ -0,0 +1,30 @@
+from selenium.common.exceptions import NoSuchFrameException
+from pages.base_page import BasePage
+
+class SwagLabs(BasePage):
+    def exist_logo(self):
+        try:
+            self.find_element(locator='div.login_logo')
+        except NoSuchFrameException:
+            return False
+        return True
+    def exist_username(self):
+        try:
+            self.find_element(locator='#login_button_container > div > form > div:nth-child(1)')
+        except NoSuchFrameException:
+            return False
+        return True
+    def exist_password(self):
+        try:
+            self.find_element(locator='#login_button_container > div > form > div:nth-child(2)')
+        except NoSuchFrameException:
+            return False
+        return True
+
+
+
+
+
+
+
+
---
+++ tests/test_check_swag.py
@@ -0,0 +1,14 @@
+from pages.swag_labs import SwagLabs
+def test_icon_exist(browser):
+    swag_labs_page = SwagLabs(browser)
+    swag_labs_page.visit()
+
+    assert swag_labs_page.exist_logo()
+    assert swag_labs_page.exist_username()
+    assert swag_labs_page.exist_password()
+
+
+
+
+
+
